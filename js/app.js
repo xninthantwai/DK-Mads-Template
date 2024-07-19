6f0af83b97935be107358b384675b707aacc48fb
xninthantwai <xninthantwai@gmail.com>
2024-07-19 23:17:08 +0630

    DK Mads's Template

---
+++ js/app.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+    reset:false,
+    distance: '30px',
+    opacity: 0,
+    duration: 1000, 
+    delay:300,
+})
+ScrollReveal().reveal(".logo", {
+    origin: "left", 
+    delay:300, 
+    interval:200,
+})
